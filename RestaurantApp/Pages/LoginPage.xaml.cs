9e87713c4c646bdb1310f48cb21b61d75537c0b8
anookala312 <anookala@mycambrian.ca>
2024-07-24 21:49:18 -0400

    LoginView Created

---
+++ RestaurantApp/Pages/LoginPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace RestaurantApp;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ RestaurantApp/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Toolkit.Mvvm.Input;
+using RestaurantApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantApp.Models
+{
+    public partial class LoginPageViewModel : BaseViewModel
+    {
+        [ObservableProperty]
+        private string _username;
+
+        [ObservableProperty]
+        private string _password;
+        
+        [ICommand]
+        public void Login() 
+        { 
+        }
+
+    }
+}
