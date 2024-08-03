12e496e907334d05464846849f9b917cbe0df9ab
anookala312 <anookala@mycambrian.ca>
2024-08-02 23:21:13 -0400

    Added MenuItemService class with predefined menu items including Butter Chicken and Fried Rice. Implemented methods to retrieve all items, get a random selection of on-sale items, and search items by name. This commit enhances menu data management and querying functionality for the restaurant app.

--- RestaurantApp/Models/MenuItems.cs
+++ RestaurantApp/Models/MenuItems.cs
@@ -7,17 +7,17 @@ using System.Threading.Tasks;
 
 namespace RestaurantApp.Models
 {
-    public partial class MenuItem
+    public partial class MenuItems
     {
-        public string Name { get; set; }
-        public string Image {  get; set; }
-        public double Price { get; set; }
+        public string? Name { get; set; }
+        public string? Image {  get; set; }
+        public double? Price { get; set; }
 
         [ObservableProperty, NotifyPropertyChangedFor(nameof(Amount))]
         private int _cartQuantity;
 
         public double Amount => CartQuantity * Price;
 
-        public MenuItem Clone() => MemberwiseClone() as MenuItem;
+        public MenuItems Clone() => MemberwiseClone() as MenuItems;
     }
 }
---
+++ RestaurantApp/Services/MenuItemService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestaurantApp.Models;
+
+namespace RestaurantApp.Services
+{
+    public class MenuItemService
+    {
+        private readonly static IEnumerable<MenuItems> _items = new 
+            List<MenuItems>()
+            {
+                new MenuItems
+                {
+                    Name = "Butter Chicken",
+                    Image = "butterchicken.png",
+                    Price = 10
+                },
+                new MenuItems
+                {
+                    Name = "Assorted Peppersoup",
+                    Image = "assortedpeppersoup.jpeg",
+                    Price = 10
+                },
+                new MenuItems
+                {
+                    Name = "Catfish Peppersoup",
+                    Image = "catfishpeppersoup.jpeg",
+                    Price = 12
+                },
+                new MenuItems
+                {
+                    Name = "Jellofrice and Plantain",
+                    Image = "jellofriceandplantain.jpeg",
+                    Price = 10
+                },
+                new MenuItems
+                {
+                    Name = "Fried Rice",
+                    Image = "friedrice.jpeg",
+                    Price = 8
+                }
+            };
+        
+        public IEnumerable<MenuItems> GetAllMenuItems() => _items;
+
+        public IEnumerable<MenuItems> GetOnSaleMenuItems(int count = 4) => _items.OrderBy(p => Guid.NewGuid()).Take(count);
+
+        public IEnumerable<MenuItems> GetMenuItems(string searchTerm) => string.IsNullOrWhiteSpace(searchTerm)? _items
+            : _items.Where(p => p.Name.Contains(searchTerm,StringComparison.OrdinalIgnoreCase));
+    }
+}
